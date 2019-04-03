b71a9a0053ee43f03cd9ac6215ce22d9a1727cb9
gadicker <gadicker@ucollege.edu>
2019-04-03 13:29:18 -0500

    Third again push

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
-  rendor() {
+  render() {
     return (
       <li><button>{this.props.button_text}</button></li>
     )
